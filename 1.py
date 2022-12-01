b4ee7fbe1d2e49bb0eb18f96d87c14325271ad33
Zahidul Islam <zahidul15-12522@diu.edu.bd>
2022-12-01 16:28:14 +0600

    Second Commit

--- 1.py
+++ 1.py
@@ -4,7 +4,7 @@ list1 = []
 list2 = []
 updatedList = []
 
-n = int(input("Enter the range of these lists : "))
+n = int(input("Enter the range of these lists : ")) # user will input the range of these lists
 
 for x in range(n):
     data1 = int(input("Enter integer value for list 1 : "))
@@ -12,7 +12,7 @@ for x in range(n):
     list1.append(data1)
     list2.append(data2)
 
-    newData = list1[x] + list2[x]
+    newData = list1[x] + list2[x] # adding values of two list index wise
     updatedList.append(newData)
 
 print("Here is the list 1 : ",list1,"\nHere is the list 2 : ",list2)
--- 3.py
+++ 3.py
@@ -2,9 +2,9 @@
 
 li = ['C', 'C++', 'Java', 'Python']
 
-n = input("Enter the item after which you want to add new item : ")
-k = li.index(n)
+n = input("Enter the item after which you want to add new item : ") # for integer item : int(input())
+k = li.index(n) # looking for the index no. of the specified item.
 data = input("Enter the new value : ")
-li.insert(k+1, data)
+li.insert(k+1, data) # adding new value after the specified item.
 
 print(li)
